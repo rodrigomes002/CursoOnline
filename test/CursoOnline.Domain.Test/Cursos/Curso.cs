63290105476f80c38dd9d8d3c6adb9c619dcb1e3
Rodrigo Gomes Calixto da Silva <rodrigo.calixto@radixeng.com.br>
2022-12-13 22:51:03 -0300

    Add project files.

---
+++ test/CursoOnline.Domain.Test/Cursos/Curso.cs
@@ -0,0 +1,19 @@
+﻿namespace CursoOnline.Domain.Test.Cursos
+{
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+
+
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        {
+            this.Nome = nome;
+            this.CargaHoraria = cargaHoraria;
+            this.PublicoAlvo = publicoAlvo;
+            this.Valor = valor;
+        }
+    }
+}
---
+++ test/CursoOnline.Domain.Test/Cursos/CursoTest.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoOnline.Domain.Test.Cursos
+{    
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            const string nome = "Informática básica";
+            const double cargaHoraria = 80;
+            const string publicoAlvo = "Estudantes";
+            const double valor = 950;
+
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+            
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+    }
+}
